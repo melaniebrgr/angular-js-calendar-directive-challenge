7a661ddb2ecfc65495e35bd72539c229d705323b
Melanie Burger <melanie.brgr@gmail.com>
2016-06-27 19:38:07 -0400

    parent controller at module level

--- app/app.js
+++ app/app.js
@@ -1,46 +1,46 @@
 angular.module('calendarDemoApp', [])
+	.controller('calendarDemoCtrl', function($scope) {
+		let preparedDate = CalendarRange.prepareDate(new Date());
+
+		$scope.months = [
+			{ name: 'January', number: 0 },
+			{ name: 'February', number: 1 },
+			{ name: 'March', number: 2 },
+			{ name: 'April', number: 3 },
+			{ name: 'May', number: 4 },
+			{ name: 'June', number: 5 },
+			{ name: 'July', number: 6 },
+			{ name: 'August', number: 7 },
+			{ name: 'September', number: 8 },
+			{ name: 'October', number: 9 },
+			{ name: 'November', number: 10 },
+			{ name: 'December', number: 11 }];
+		$scope.years = [];
+		for (let i = preparedDate.year - 20; i <= preparedDate.year + 20; i++) {
+			$scope.years.push(i);
+		}
+
+		$scope.selectedMonth = $scope.months[preparedDate.month];
+		$scope.selectedYear = $scope.years[parseInt($scope.years.length/2)];	
+	})
 	.directive('calendarMenu', function() { 
 		return {
 			restrict: 'E',
 			templateUrl: 'calendar-menu.html',
-			link: function(scope, element, attrs) {
-				let preparedDate = CalendarRange.prepareDate(new Date());
-				console.log(preparedDate.year);
-
-				scope.months = [
-					{ name: 'January', number: 1 },
-					{ name: 'February', number: 2 },
-					{ name: 'March', number: 3 },
-					{ name: 'April', number: 4 },
-					{ name: 'May', number: 5 },
-					{ name: 'June', number: 6 },
-					{ name: 'July', number: 7 },
-					{ name: 'August', number: 8 },
-					{ name: 'September', number: 9 },
-					{ name: 'October', number: 10 },
-					{ name: 'November', number: 11 },
-					{ name: 'December', number: 12 }];
-				
-				scope.selectedMonth = scope.months[preparedDate.month];
-
-				scope.years = [];
-				for (let i = preparedDate.year - 20; i <= preparedDate.year + 20; i++) {
-					scope.years.push(i);
-				}
-				scope.selectedYear = scope.years[parseInt(scope.years.length/2)];
-			}
+			controller: 'calendarDemoCtrl'
 		}
 	})
 	.directive('calendarDisplay', function() {
 		return {
 			restrict: 'E',
 			templateUrl: 'calendar-display.html',
+			controller: 'calendarDemoCtrl',
 			link: function(scope, element, attrs) {
-				scope.today = new Date();
-				scope.preparedDate = CalendarRange.prepareDate(scope.today);
-				scope.monthlyRange = CalendarRange.getMonthlyRange(scope.today);
-				// console.log(scope.preparedDate.month)
-				// console.log(scope.monthlyRange.days[0].month);
+				// When month or year changes, update calendar
+
+				scope.selectedDate = new Date(scope.selectedYear, scope.selectedMonth.number);
+				scope.preparedDate = CalendarRange.prepareDate(scope.selectedDate);
+				scope.monthlyRange = CalendarRange.getMonthlyRange(scope.selectedDate);
 			}
 		}
 	});
